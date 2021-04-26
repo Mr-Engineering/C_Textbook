a66d6a0c7d0ff61f889fa9a8b16c3420df6aa498
Mr-Engineering <Mr-Engineering@users.noreply.github.com>
2021-04-27 06:33:02 +0900

    과제 진행중

--- Textbook/Ex1_2.c
+++ Textbook/Ex1_2.c
@@ -1,25 +1,26 @@
 // 예제 1-2
 
-#include <stdio.h> // printf()와 scanf()를 사용하기 위한 헤더 파일 포함
+#include <stdio.h>				// printf()와 scanf()등 표준 함수를 사용하기 위한 헤더 파일 stdio.h 호출
 #pragma warning(disable:4996)  // 권장하지 않는 함수 사용에 대한 경고 메세지 무시
 
-int a, b, c; // 정수를 저장하는 변수 선언
-int product(int x, int y);  // 사용자 함수 product 원형 --> 왜 있을까?
+int a, b, c;				 // 정수를 저장하는 변수 a, b, c 선언
+int product(int x, int y);  // 사용자 함수 product 원형 선언 : 반환값의 자료형은 int(정수형), 입력 변수 x, y의 자료형 모두 int(정수형)
 
-void main() {
-	// 첫 번째 수 a 입력
-	printf("Enter a number between 1 and 100 : ");
-	scanf("%d", &a); // a라는 변수가 저장된 공간의 주소에 %d 의 숫자(int)를 입력하는 경우 &을 넣음. 문자열의 경우 문자열 자체가 주소임
+void main() {				// 반환형이 없는(void) 메인 함수 선언
+	/* 첫 번째 수를 입력*/
+	printf("Enter a number between 1 and 100 : "); // "Enter a number between 1 and 100 : "를 실행 화면에 출력
+	scanf("%d", &a); // scanf 함수 : 지정한 형식에 맞춰 내용을 입력하면, 함수에서 지정한 형태의 자료형으로 
+					 // a라는 변수가 저장된 공간의 주소에 %d 의 숫자(int)를 입력. scanf를 &을 넣음. 
 
-	// 두 번째 수 b 입력
-	printf("Enter another number between 1 and 100 : ");
-	scanf("%d", &b);
+	/* 두 번째 수를 입력*/
+	printf("Enter another number between 1 and 100 : "); // "Enter a number between 1 and 100 : "를 실행 화면에 출력
+	scanf("%d", &b); // a라는 변수가 저장된 공간의 주소에 %d 의 숫자(int)를 입력하는 경우 &을 넣음.
 
-	// 함수 호출 및 두 수를 곱하여 결과 출력
-	c = product(a, b);
-	printf("%d * %d = %d \n", a, b, c);
+	/* 함수 호출에 의해 두 수를 곱하고 결과를 출력*/
+	c = product(a, b); // product 함수를 통해 정수 a, b를 더하고 결과값을 c에 저장
+	printf("%d * %d = %d \n", a, b, c); // 
 }
-
-int product(int x, int y) {
-	return (x * y);
+/* 두 수의 곱을 계산하여 돌려주는 사용자 함수*/
+int product(int x, int y) { // 함수 product의 내용 정의
+	return (x * y);			// 함수 product의 결과로 정수 x와 정수 y의 곱을 반환(return)
 }
--- Textbook/Ex2_2.c
+++ Textbook/Ex2_2.c
@@ -1,13 +1,13 @@
 // 예제 2-2
 
-#include <stdio.h>
-#define IN "원의 반지름은 100입니다. \n"
-#define PI 3.1415926535
-#define AREA(X) (PI*(X)*(X))
-#define OUT printf("원의 면적을 구하고 프로그램을 종료합니다.\n")
-void main()
-{
-	printf("%s \n" IN);
-	printf("원의 면적은 %10.5f입니다. \n\n", AREA(100));
-	OUT;
-}
+//#include <stdio.h>
+//#define IN "원의 반지름은 100입니다. \n"  // 문자열과 줄바꿈(\n)을 IN에 입력
+//#define PI 3.1415926535					// 3.1415925635를 PI에 입력
+//#define AREA(X) (PI*(X)*(X))				// 변수 X와 상수 PI에 대해 원의 넓이를 구하는 함수인 AREA(X) 선언
+//#define OUT printf("원의 면적을 구하고 프로그램을 종료합니다.\n")	// 문자열과 줄바꿈(\n)을 실행 화면에 출력하는 함수를 OUT에 입력
+//void main()
+//{
+//	printf("%s \n" IN);
+//	printf("원의 면적은 %10.5f입니다. \n\n", AREA(100));
+//	OUT;
+//}
